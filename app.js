448dbcbba1410d4cb918785b8ed3595046216455
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-23 16:08:39 +0200

    Set up middleware in app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const initializePassport = require("./passport-config");
+
+require("dotenv").config();
+
+const app = express();
+
+initializePassport(passport);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || "keyboard cat",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
+
