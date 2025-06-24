6f92be79b24b3c47d2a00a8cfefcd441b6e305d2
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-24 10:19:01 +0200

    Add missing functions to messageController

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -17,3 +17,22 @@ exports.index = async (req, res, next) => {
     next(err);
   }
 };
+
+// Add these missing functions:
+exports.new_message_get = (req, res) => {
+  res.render("new-message");
+};
+
+exports.new_message_post = async (req, res, next) => {
+  try {
+    // Add your message creation logic here
+    const { title, text } = req.body;
+    await db.query(
+      'INSERT INTO messages (title, text, user_id, timestamp) VALUES ($1, $2, $3, NOW())',
+      [title, text, req.user.id]
+    );
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+};
