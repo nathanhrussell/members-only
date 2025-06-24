6c57cf9ac604581a424d96652e983a6b9530e72c
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-24 09:59:40 +0200

    Add ensureClubMember function to auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,3 +5,12 @@ exports.ensureAuthenticated = (req, res, next) => {
   req.flash("error", "You must be logged in to view this page");
   res.redirect("/login");
 };
+
+exports.ensureClubMember = (req, res, next) => {
+  const status = req.user?.membership_status;
+  if (status === "club_member" || status === "admin") {
+    return next();
+  }
+  req.flash("error", "Only club members can post messages");
+  res.redirect("/join-club");
+};
